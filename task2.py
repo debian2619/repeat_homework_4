d9b9d3a7721511a05aa4bae9ef557f5af5488e4f
Dmitry <ya.potapov@yandex.ru>
2022-10-09 15:38:35 +0300

    task2.py

---
+++ task2.py
@@ -0,0 +1,23 @@
+2 Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+# *Пример*
+
+# - при N=236     ->        [2, 2, 59]
+import math  
+def prime_factors(num):  
+    
+    while num % 2 == 0:  
+        print(2,)  
+        num = num / 2  
+  
+    for i in range(3, int(math.sqrt(num)) + 1, 2):  
+  
+        while num % i == 0:  
+            print(i,)  
+            num = num / i  
+    if num > 2:  
+        print(num)  
+  
+  
+num = 236 
+prime_factors(num)  
